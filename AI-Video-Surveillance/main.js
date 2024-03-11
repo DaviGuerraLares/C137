6eb90a9a934e22f05ccc265fecaf73e53a63a015
Davi <123010157+DaviGuerraLares@users.noreply.github.com>
2024-03-11 15:41:08 -0300

    Add files via upload

---
+++ AI-Video-Surveillance/main.js
@@ -0,0 +1,45 @@
+objects = []
+status = ""
+video = "";
+function preload() {
+    video = createVideo("video.mp4")
+    video.hide()
+}
+function setup() {
+    canvas = createCanvas(480, 380)
+    canvas.center()
+}
+function draw() {
+    image(video, 0, 0, 480, 380)
+    if (status != "") {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objetos Detectados";
+            document.getElementById("numberOfObjects").innerHTML = "Quantidade de Objetos Detectados: " + objects.length
+            fill("#FF0000");
+            pedrcent = floor(objects[i].confidence * 100);
+            Text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function start() {
+    objectdetector = ml5.objectDetector("cocossd", modelLoaded)
+    document.getElementById("status").innerHTML = "Status: Detectando Objetos"
+}
+function modelLoaded() {
+    console.log("Modelo Carregado!")
+    status = true;
+    video.loop();
+    video.speed();
+    video.volume();
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error)
+    }
+    console.log(results)
+    objects = results
+}
